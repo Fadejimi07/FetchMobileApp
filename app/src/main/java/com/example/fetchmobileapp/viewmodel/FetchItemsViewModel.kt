d5cf6a70d65f56fa27cbda4de29a6a22ca8642cd
Fadejimi <adegbulugbefadejimi@gmail.com>
2025-02-28 16:01:18 -0600

    Initialize Viewmodel

---
+++ app/src/main/java/com/example/fetchmobileapp/viewmodel/FetchItemsViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.fetchmobileapp.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.fetchmobileapp.model.Item
+import com.example.fetchmobileapp.repository.FetchItemsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FetchItemsViewModel @Inject constructor(private val repo: FetchItemsRepository) :
+    ViewModel() {
+    var items by mutableStateOf<Map<Int, List<Item>>>(emptyMap())
+        private set
+
+    init {
+        viewModelScope.launch {
+            val itemsCountInDB = repo.getItemsCountInDB()
+            if (itemsCountInDB > 0) {
+                fetchItemsFromDB()
+            } else {
+                val onlineItems = repo.fetchItemsFromOnlineApi()
+                repo.insertItemsIntoDB(onlineItems)
+                fetchItemsFromDB()
+            }
+        }
+    }
+
+    private fun fetchItemsFromDB() {
+        viewModelScope.launch {
+            val itemsListIds = repo.getListIdsFromDB()
+            val itemMap: MutableMap<Int, List<Item>> = mutableMapOf()
+            for (listId in itemsListIds) {
+                itemMap[listId] = repo.getItemsByListIdFromDB(listId)
+            }
+            items = itemMap
+        }
+    }
+}

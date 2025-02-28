c1fb5e0ed5e4ff61666b160f3fdeb5d6b406d814
Fadejimi <adegbulugbefadejimi@gmail.com>
2025-02-28 16:08:19 -0600

    Display the header item

---
+++ app/src/main/java/com/example/fetchmobileapp/screens/ItemHeader.kt
@@ -0,0 +1,29 @@
+package com.example.fetchmobileapp.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HeaderItem(listId: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.Red)
+    ) {
+        Text(
+            text = listId,
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+        )
+    }
+}

bf945924455b2362f0a995d777b56cc1dddf6415
Fadejimi <adegbulugbefadejimi@gmail.com>
2025-02-28 15:41:43 -0600

    Repository Integration

--- app/src/main/java/com/example/fetchmobileapp/repository/FetchItemsRepository.kt
+++ app/src/main/java/com/example/fetchmobileapp/repository/FetchItemsRepository.kt
@@ -1,4 +1,15 @@
 package com.example.fetchmobileapp.repository
 
+import com.example.fetchmobileapp.model.Item
+
 interface FetchItemsRepository {
+    suspend fun fetchItemsFromOnlineApi(): List<Item>
+
+    suspend fun insertItemsIntoDB(items: List<Item>)
+
+    suspend fun getListIdsFromDB(): List<Int>
+
+    suspend fun getItemsByListIdFromDB(listId: Int): List<Item>
+
+    suspend fun getItemsCountInDB(): Int
 }
---
+++ app/src/main/java/com/example/fetchmobileapp/repository/FetchItemsRepositoryImpl.kt
@@ -0,0 +1,32 @@
+package com.example.fetchmobileapp.repository
+
+import com.example.fetchmobileapp.model.Item
+import com.example.fetchmobileapp.network.ApiService
+import com.example.fetchmobileapp.room.ItemDao
+import javax.inject.Inject
+
+class FetchItemsRepositoryImpl @Inject constructor(
+    private val itemDao: ItemDao,
+    private val apiService: ApiService
+) :
+    FetchItemsRepository {
+    override suspend fun fetchItemsFromOnlineApi(): List<Item> {
+        return apiService.getList()
+    }
+
+    override suspend fun insertItemsIntoDB(items: List<Item>) {
+        itemDao.insertItemList(items)
+    }
+
+    override suspend fun getListIdsFromDB(): List<Int> {
+        return itemDao.getListIds()
+    }
+
+    override suspend fun getItemsByListIdFromDB(listId: Int): List<Item> {
+        return itemDao.getItemsFromList(listId)
+    }
+
+    override suspend fun getItemsCountInDB(): Int {
+        return itemDao.getItemCountFromTable()
+    }
+}
